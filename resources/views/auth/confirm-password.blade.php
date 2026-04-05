af2fb615c1a4cf1aa20cceecc72fc68574e82dfb
Trisha <maicotrishaangela@gmail.com>
2026-04-05 10:46:24 +0800

    nag add lang sariling database ni maico

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -3,7 +3,7 @@
         {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
     </div>
 
-    <form method="POST" action="{{ route('password.confirm') }}">
+    <form method="POST" action="/confirm-password">
         @csrf
 
         <!-- Password -->
@@ -20,7 +20,7 @@
 
         <div class="flex justify-end mt-4">
             <x-primary-button>
-                {{ __('Confirm') }}
+                Confirm
             </x-primary-button>
         </div>
     </form>
